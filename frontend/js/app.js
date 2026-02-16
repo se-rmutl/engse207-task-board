c459155aac7c237b841f88de14834e753bb0088c
tkeatkaew <thanit@rmutl.ac.th>
2026-02-16 12:11:58 +0700

    frontend updated

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -3,7 +3,7 @@
 // ENGSE207 - Week 6 Docker Version
 // ============================================
 
-const API_BASE = 'https://engse207-task-board-production.up.railway.app/api';
+const API_BASE = 'https://engse207-task-board-production-1531.up.railway.app/api';
 
 // ============================================
 // API Functions
